f8443822eec784a4cd8c19b02af32b814e0617b5
clafuenteEurotech <clafuente@eurotechtls.eu>
2023-06-26 16:49:02 +0200

    Commit inicial

---
+++ src/main/java/eu/eurotechtls/simusimple/SimusimpleApplication.java
@@ -0,0 +1,13 @@
+package eu.eurotechtls.simusimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimusimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimusimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/eurotechtls/simusimple/SimusimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.eurotechtls.simusimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimusimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
